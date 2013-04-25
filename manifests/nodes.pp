3da6a91a42d44d424519bb07f7daa9f6b40f925a
Kim Brugger <kb468@cam.ac.uk>
2013-04-25 16:37:15 +0100

    updated nodes file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,9 +1,12 @@
 node base {
+    
+    include users
+
     include ntp
     include tcsh
     include git
     include sudo
-    include mdadm
+    include hosts
 }
 
 node work inherits base {
@@ -11,7 +14,25 @@ node work inherits base {
    include gitk
 }
 
+node headnode inherits work {
+   include mdadm
+   include "gridengine-master"
+   include "gridengine-qmon"
+   include "gridengine-client"
+   include augeas
+   include "nfs-server"
+}
 
-node 'mgsrv13.medschl.cam.ac.uk' inherits work {
+node execnode inherits base {
+   include gridengine-exec
+   include nfs-client
+}
+
+node compute-100-1 inherits execnode {
+
+}
+
+node 'mgsrv13.medschl.cam.ac.uk' inherits headnode {
+
 }
 
