e72c04dd5c4afb29bd1e80aa9fc629b7c9a0a2c1
Kim Brugger <kb468@cam.ac.uk>
2013-05-21 12:21:22 +0100

    /etc/exports now a file, and not dynammically changed

--- modules/nfs-server/manifests/init.pp
+++ modules/nfs-server/manifests/init.pp
@@ -2,48 +2,14 @@ class nfs-server {
    
   package { "nfs-kernel-server": status => latest }
 
-  augeas{ "exports" : 
-    context => "/files/etc/exports",
-    changes => [ 
-        "set dir[. = '/data'] /data",
-        "set dir[. = '/data']/client 10.1.0.0/255.255.0.0",
-        "set dir[. = '/data']/client/option[1] rw",
-        "set dir[. = '/data']/client/option[2] no_subtree_check",
-        "set dir[. = '/data']/client/option[3] async",
-        "set dir[. = '/data']/client/option[4] no_root_squash",
 
-        "set dir[. = '/home'] /home",
-        "set dir[. = '/home']/client 10.1.0.0/255.255.0.0",
-        "set dir[. = '/home']/client/option[1] rw",
-        "set dir[. = '/home']/client/option[2] no_subtree_check",
-        "set dir[. = '/home']/client/option[4] no_root_squash",
-
-        "set dir[. = '/seqs'] /seqs",
-        "set dir[. = '/seqs']/client 10.1.0.0/255.255.0.0",
-        "set dir[. = '/seqs']/client/option[1] rw",
-        "set dir[. = '/seqs']/client/option[2] no_subtree_check",
-        "set dir[. = '/seqs']/client/option[4] no_root_squash",
-
-        "set dir[. = '/software'] /software",
-        "set dir[. = '/software']/client 10.1.0.0/255.255.0.0",
-        "set dir[. = '/software']/client/option[1] rw",
-        "set dir[. = '/software']/client/option[2] no_subtree_check",
-        "set dir[. = '/software']/client/option[4] no_root_squash",
-
-        "set dir[. = '/refs'] /refs",
-        "set dir[. = '/refs']/client 10.1.0.0/255.255.0.0",
-        "set dir[. = '/refs']/client/option[1] rw",
-        "set dir[. = '/refs']/client/option[2] no_subtree_check",
-        "set dir[. = '/refs']/client/option[4] no_root_squash",
-
-
-
-    ],
-  } 
+  file {'/etc/exports' : source => "puppet:///modules/nfs-server/exports", 
+                         mode   => 755,
+                         notify => Service['nfs-kernel-server']}
 
    service { "nfs-kernel-server":
         ensure => running,
-        require => Package["nfs-kernel-server"],
+        require => [Package["nfs-kernel-server"], File['/etc/exports']],
    }
 
 
