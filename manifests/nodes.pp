6b7a47027b51c9851b16a9d147c3b5b3e82539c8
Kim Brugger <kb468@cam.ac.uk>
2013-05-21 11:48:42 +0100

    new modules added

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,11 +7,18 @@ node base {
     include git
     include sudo
     include hosts
+
+    include perl
+    include python
+    include r
+    include java
 }
 
 node work inherits base {
    include emacs
    include gitk
+   include vim
+   include development
 }
 
 node headnode inherits work {
@@ -24,13 +31,19 @@ node headnode inherits work {
 }
 
 node execnode inherits base {
-   include gridengine-exec
    include nfs-client
+   include gridengine::execd
 }
 
-node compute-100-1 inherits execnode {
+node compute-100-1 inherits execnode {}
+node compute-100-2 inherits execnode {}
+node compute-100-3 inherits execnode {}
+node compute-100-4 inherits execnode {}
+node compute-100-5 inherits execnode {}
+node compute-100-6 inherits execnode {}
+node compute-100-7 inherits execnode {}
+node compute-100-8 inherits execnode {}
 
-}
 
 node 'mgsrv13.medschl.cam.ac.uk' inherits headnode {
 
